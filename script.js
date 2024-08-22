01676ca5fe365cfb2f2a424b280318ff0f04fb31
Charmie-Goel <charmiegoyal89@gmail.com>
2024-08-22 23:17:08 +0530

    update

---
+++ script.js
@@ -0,0 +1,29 @@
+let student = {
+  id: 101,
+  name: "charmie", // object creation and updating the values of keys
+};
+// console.log(student["id"]);
+// student.id = 106;
+// student["name"] = "krish";
+// console.log(student.id)
+// console.log(student["name"]);
+// student.age = 23;
+// console.log(student.age);
+
+/*console.log(student);
+delete student.id;                                              // deleting the key value pair of an object
+console.log(student);*/
+
+// let employee = {
+//   eid: 101,
+//   ename: "Rajeev",
+//   isEmployee: true, //nested object
+//   exp: {
+//     oracle: 2,
+//     google: 3,
+//     microsoft: 1,
+//     deloit: 2,
+//   },
+// };
+// console.log(employee.eid);
+// console.log(employee.exp.google);          // printing the key value pair of nested object
